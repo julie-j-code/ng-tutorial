e6edb2e05776cfb607278570fa397388e26ddfc8
Julie J <jeannet.julie@gmail.com>
2022-01-22 10:33:08 +0100

    angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { pathToFileURL } from 'url';
+import { BlogComponent } from "./components/blog/blog.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { CoursesComponent } from "./components/courses/courses.component";
+import { HomeComponent } from "./components/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'home', component:HomeComponent},
+{path:'blog', component:BlogComponent},
+{path:'courses', component:CoursesComponent},
+{path:'contact', component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,21 @@ import { UsersModule } from './users/users.module';
 import { ProductsComponent } from './products/products.component';
 import { HideElementsDirective } from './hide-elements.directive';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     ProductsComponent,
-    HideElementsDirective
+    HideElementsDirective,
+    HomeComponent,
+    ContactComponent,
+    CoursesComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
